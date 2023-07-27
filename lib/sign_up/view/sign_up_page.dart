3c2535f2ef68024625afa9b483e2dd2bccfbc8a9
Roger Bacab <roger.bacab7@gmail.com>
2023-07-27 17:37:24 -0600

    adding sign up

---
+++ lib/sign_up/view/sign_up_page.dart
@@ -0,0 +1,28 @@
+import 'package:authentication_repository/authentication_repository_exports.dart';
+import 'package:firebase_bloc_login_example/sign_up/sign_up_exports.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  static Route<void> route() {
+    return MaterialPageRoute<void>(builder: (_) => const SignUpPage());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: BlocProvider<SignUpCubit>(
+          create: (_) => SignUpCubit(context.read<AuthenticationRepository>()),
+          child: const SignUpForm(),
+        ),
+      ),
+    );
+  }
+}
